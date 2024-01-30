f74ff6d0ef825c639a0ffb22591d5d5026fd22aa
Nelson Ortega <ne.beltran18@gmail.com>
2024-01-29 20:08:48 -0500

    feat: implementation of main menu for the application console

---
+++ AirportBooking/Interface.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportBooking
+{
+    public class Interface
+    {
+        public User? CurrentUser { get; set; }
+        
+        
+        private void ShowLoginMenu()
+        {
+            Console.WriteLine("Login menu");
+        }
+
+        public void ShowMainMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("""
+                - ############################################## -
+
+                Welcome to the Airport's Booking Management system
+                To continue please select one of the options below
+
+                - ############################################## -
+
+                1. Login
+                2. Exit
+                """);
+                string? answer = Console.ReadLine();
+                Console.Clear();
+                switch (answer)
+                {
+                    case "1":
+                        ShowLoginMenu();
+                        break;
+                    case "2":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input, please try again");
+                        Console.ReadLine();
+                        break;
+                };
+                Console.Clear();
+            }
+        }
+    }
+}
--- AirportBooking/Program.cs
+++ AirportBooking/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AirportBooking;
+
+Interface userInterface = new();
+userInterface.ShowMainMenu();
