4344cce056ce96c7429dada3a64f52ccec5aa82a
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-02-07 17:20:20 -0500

    fix: separated booking csv validation logic

--- AirportBooking/Serializers/CSVSerializers/BookingCsvSerializer.cs
+++ AirportBooking/Serializers/CSVSerializers/BookingCsvSerializer.cs
@@ -1,15 +1,16 @@
 ﻿using AirportBooking.Enums;
 using AirportBooking.Exceptions;
 using AirportBooking.Models;
+using AirportBooking.Validators.CsvValidators;
 
 namespace AirportBooking.Serializers.CSVSerializers;
 
 public class BookingCsvSerializer : ICSVSerializer<Booking>
 {
+    private readonly BookingCsvValidator _validator = new();
     public Booking FromCsv(string csvLine)
     {
-        ValidateCsvLine(csvLine);
-        var data = csvLine.Split(",");
+        var data = _validator.Validate(csvLine);
         (var reservationNumber, var bookingType) = (int.Parse(data[0]), Enum.Parse<BookingType>(data[1]));
         var flightClass1 = Enum.Parse<FlightClass>(data[2]);
         var flightClasses = new List<FlightClass> { flightClass1 };
@@ -25,16 +26,6 @@ public class BookingCsvSerializer : ICSVSerializer<Booking>
             BookingType = bookingType,
             MainPassenger = new User { Username = username }
         };
-    }
-
-    private static void ValidateCsvLine(string csvLine)
-    {
-        var data = csvLine.Split(",");
-        var error = new EntitySerializationException<Booking>($"Incomplete data on line {csvLine}");
-        if (data.Length < 2) throw error;
-        var bookingType = Enum.Parse<BookingType>(data[1]);
-        if (bookingType is BookingType.OneWay && data.Length < 4) throw error;
-        if (bookingType is BookingType.RoundTrip && data.Length < 5) throw error;
     }
 
     public string ToCsv(Booking booking)
---
+++ AirportBooking/Validators/CsvValidators/BookingCsvValidator.cs
@@ -0,0 +1,19 @@
+﻿using AirportBooking.Enums;
+using AirportBooking.Exceptions;
+using AirportBooking.Models;
+
+namespace AirportBooking.Validators.CsvValidators;
+
+public class BookingCsvValidator : ICsvValidator
+{
+    public string[] Validate(string csvLine)
+    {
+        var data = csvLine.Split(",");
+        var error = new EntitySerializationException<Booking>($"Incomplete data on line {csvLine}");
+        if (data.Length < 2) throw error;
+        var bookingType = Enum.Parse<BookingType>(data[1]);
+        if (bookingType is BookingType.OneWay && data.Length < 4) throw error;
+        if (bookingType is BookingType.RoundTrip && data.Length < 5) throw error;
+        return data;
+    }
+}
