1d45d180a0296991843f3e94893f765faef17087
Nelson Ortega <nelson.ortega@foothillsolutions.com>
2024-02-10 16:01:51 -0500

    feat: implemented logic for batch upload of flights

--- AirportBooking/Views/ManagerView.cs
+++ AirportBooking/Views/ManagerView.cs
@@ -1,14 +1,24 @@
-﻿using AirportBooking.Views.Controllers;
+﻿using AirportBooking.Exceptions;
+using AirportBooking.FileReaders;
+using AirportBooking.Models;
+using AirportBooking.Serializers.CSVSerializers;
+using AirportBooking.Views.Controllers;
 
 namespace AirportBooking.Views;
 
 public class ManagerView : RoleConsoleView
 {
     private readonly BookingsController _bookingsController;
+    private readonly FlightsController _flightsController;
+    private readonly CSVReader _csvReader;
+    private readonly FlightCsvSerializer _serializer;
 
-    public ManagerView(BookingsController controller)
+    public ManagerView(BookingsController controller, CSVReader csvReader, FlightsController flightsController, FlightCsvSerializer serializer)
     {
         _bookingsController = controller;
+        _csvReader = csvReader;
+        _flightsController = flightsController;
+        _serializer = serializer;
     }
 
     public override void ShowMenu()
@@ -38,11 +48,57 @@ public class ManagerView : RoleConsoleView
 
     private void UploadFlights()
     {
-        throw new NotImplementedException();
+        while (true)
+        {
+            var rootPath = Directory.GetParent(Path.Combine("..", ".."))!.FullName;
+            var flightsPath = Path.Combine(rootPath, "Data", "flights");
+            var existingFiles = Directory.GetFiles(flightsPath, "*.csv");
+            Console.WriteLine("Please upload the flight data inside the Data directory at the flights folder. The file must be a csv");
+            Console.WriteLine("Press enter once you have placed the new data");
+            ClearOnInput();
+            var newFiles = Directory.GetFiles(flightsPath, "*.csv");
+            if (!(newFiles.Length > existingFiles.Length))
+            {
+                Console.WriteLine("No file has been introduced. Please try again");
+                return;
+            }
+            var latestFile = newFiles[^1];
+            string[] uploadedFlights = File.ReadAllLines(latestFile);
+            var errorList = new List<Exception>();
+            var originalFlightList = _flightsController.FindAll().ToList();
+            foreach (var line in uploadedFlights)
+            {
+                try
+                {
+                    _flightsController.Create(_serializer.FromCsv(line));
+                }
+                catch (Exception ex) when (ex is EntitySerializationException<Flight>)
+                {
+                    errorList.Add(ex);
+                }
+            }
+            if (errorList.Count == 0) return;
+            foreach (var error in errorList)
+            {
+                Console.WriteLine(error.Message);
+            }
+            _flightsController.FindAll().ToList().ForEach(f => _flightsController.Delete(f.Number));
+            originalFlightList.ForEach(f => _flightsController.Create(f));
+        }
     }
 
     private void FilterBookings()
     {
-        throw new NotImplementedException();
+        var filter = _bookingsController.ObtainFilterParameters();
+        var bookings = _bookingsController.FilterByParameters(filter) ?? [];
+        if (bookings.Count == 0)
+        {
+            Console.WriteLine("No bookings matched your characteristics");
+            return;
+        }
+        foreach (var booking in bookings)
+        {
+            _bookingsController.PrintToConsole(booking);
+        }
     }
 }
